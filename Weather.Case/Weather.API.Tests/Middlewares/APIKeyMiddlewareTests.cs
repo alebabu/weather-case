fbdf497c8496c743e07c3af175331295467be503
alebabu <aleksandarbabunovic@gmail.com>
2025-11-03 14:28:42 +0100

    Added Tests for APIKeyMiddleware

---
+++ Weather.Case/Weather.API.Tests/Middlewares/APIKeyMiddlewareTests.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Weather.API.Middlewares;
+
+namespace Weather.API.Tests.Middlewares
+{
+    public class APIKeyMiddlewareTests
+    {
+        private readonly HttpContext context;
+        private readonly RequestDelegate requestDelegate;
+        private readonly APIKeyMiddleware middleware;
+
+        private bool requestWasCalled;
+
+        public APIKeyMiddlewareTests()
+        {
+            // Set up HttpContext
+            context = new DefaultHttpContext();
+
+            var configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string?>
+                {
+                    { "X-API-KEY", "valid-key" }
+                })
+                .Build();
+
+            var spMock = new Mock<IServiceProvider>();
+            
+            spMock
+                .Setup(p => p.GetService(typeof(IConfiguration)))
+                .Returns(configuration);
+
+            context.RequestServices = spMock.Object;
+
+            // Set up request delegate
+            requestDelegate = _ =>
+            {
+                requestWasCalled = true;
+                return Task.CompletedTask;
+            };
+
+            // Initialize middleware
+            middleware = new APIKeyMiddleware(requestDelegate);
+        }
+
+        [Fact]
+        public async Task NoKey_Returns401()
+        {
+            await middleware.InvokeAsync(context);
+
+            Assert.True(requestWasCalled);
+            Assert.Equal(401, context.Response.StatusCode);
+        }
+
+        [Fact]
+        public async Task InvalidKey_Returns401()
+        {
+            context.Request.Headers["X-API-Key"] = "invalid-key";
+
+            await middleware.InvokeAsync(context);
+
+            Assert.True(requestWasCalled);
+            Assert.Equal(401, context.Response.StatusCode);
+        }
+
+        [Fact]
+        public async Task ValidKey_Returns200()
+        {
+            context.Request.Headers["X-API-Key"] = "valid-key";
+
+            await middleware.InvokeAsync(context);
+
+            Assert.True(requestWasCalled);
+            Assert.Equal(200, context.Response.StatusCode);
+        }
+    }
+}
